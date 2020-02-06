90a88ee1e175de6ab748af0392bb8bd536757aa3
nasciba <amandanasciba@gmail.com>
2020-02-06 15:35:38 -0300

    Initial setup - create-react-app

---
+++ florie/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+        <p>
+         Teste
+        </p>
+        
+
+    </div>
+  );
+}
+
+export default App;
