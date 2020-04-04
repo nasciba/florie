15d85dbe50378f78a3757ea9b2de206c12f496df
nasciba <amandanasciba@gmail.com>
2020-04-04 17:09:23 -0300

    CardProduct added to Products component

--- src/components/products-categories-catalog/Products.js
+++ src/components/products-categories-catalog/Products.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom';
+import CardProduct from '../products-card/CardProduct'
 import { StyledTitle } from './styles';
-import { StyledGreenButton } from '../buttons/styles';
-import {
-  StyledDisplay, StyledCardProduct, StyledTextTitle, 
-  StyledTextBrand, StyledPrice, StyledMessage
-} from '../../styles-catalog';
+import {  StyledDisplay } from '../../styles-catalog';
 
 export default class Products extends Component {
   constructor(props) {
@@ -21,21 +17,7 @@ export default class Products extends Component {
           <StyledTitle style={{ width: '80%' }}><span>{this.props.listOfProducts[0].type}</span></StyledTitle>}
         {this.props.listOfProducts.map(product => {
           return (
-            <StyledCardProduct key={product._id}>
-              <Link to={`/products/${product._id}`}>
-                <img src={product.imageUrl} alt={product.title && product.brand}></img>
-              </Link>
-              <StyledTextTitle>{product.name}</StyledTextTitle>
-              <StyledTextBrand>
-                {product.brand}
-              </StyledTextBrand>
-              <StyledPrice>
-                R${parseFloat(product.price).toFixed(2).replace('.', ',')}
-              </StyledPrice>
-              <StyledGreenButton onClick={() => { this.props.addItemToCart(product._id) }}>ADICIONAR AO CARRINHO</StyledGreenButton>
-              {product.stock <= 5 && product.stock >= 2 ? <StyledMessage> Últimas {product.stock} unidades   </StyledMessage> : null}
-              {product.stock === 1 ? <StyledMessage> Última unidade!   </StyledMessage> : null}
-            </ StyledCardProduct>
+            <CardProduct product={product} key={product._id}/>
           )
         })
         }
